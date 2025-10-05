9b155e8133044c8eff5681b9f7ac7dd222c7b0cf
vivianagh <viviana.gh@gmail.com>
2025-10-05 15:41:13 +0100

    Add baseline Spring Boot + H2 config

---
+++ src/main/java/io/github/vivianagh/jobberwocky/JobberwockyApplication.java
@@ -0,0 +1,13 @@
+package io.github.vivianagh.jobberwocky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobberwockyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobberwockyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/vivianagh/jobberwocky/JobberwockyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vivianagh.jobberwocky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobberwockyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
